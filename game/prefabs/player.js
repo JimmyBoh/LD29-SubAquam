8108db49eeaaee1c1f4126b300e1d315ec14af12
jimmyboh <jim@jimmyboh.com>
2014-04-26 16:05:18 -0400

    Fixed player jumping.

--- game/prefabs/player.js
+++ game/prefabs/player.js
@@ -13,14 +13,11 @@ var Player = function (game, x, y) {
 
 	this.isDragging = false;
 
-	//this.body.angularVelocity = -1;
-
 	this.arrow = this.game.add.sprite(-100, -100, 'arrow');
 	this.game.physics.p2.enableBody(this.arrow, false);
 	this.arrow.anchor.setTo(0.9, 0.5);
-	//this.arrow.alpha = 0;
+	this.arrow.alpha = 0;
 	this.arrow.body.static = true;
-
 
 	this.game.input.onDown.add(this._onInputDown, this);
 	this.game.input.onUp.add(this._onInputUp, this);
@@ -29,6 +26,11 @@ var Player = function (game, x, y) {
 	this.stiffness = 5;
 	this.spring.stiffness = 0;
 
+	this.isUnderwater = false;
+	this.isAbovewater = true;
+
+	this._stopDragging();
+
 	this.game.camera.follow(this, Phaser.Camera.PLATFORMER);
 };
 
@@ -36,12 +38,29 @@ Player.prototype = Object.create(Phaser.Sprite.prototype);
 Player.prototype.constructor = Player;
 
 Player.prototype.update = function () {
+
+	this.anchor.setTo(0.9, 0.5);
+
+	this.isUnderwater = this.y > this.game.height;
+	this.isAbovewater = !this.isUnderwater;
+
+
+	if (this.wasUnderwater && this.isAbovewater)
+		this._stopDragging();
+
+	if (this.wasAbovewater && this.isUnderwater && this.game.input.activePointer.isDown)
+		this._startDragging(this.game.input.activePointer);
+
+
 	this._updateGravity();
 	this._updateArrow();
 
 	this._updateInput();
 
 	this._fixVelocity();
+
+	this.wasUnderwater = this.isUnderwater;
+	this.wasAbovewater = this.isAbovewater;
 };
 
 Player.prototype._onInputDown = function (pointer, e) {
@@ -60,13 +79,17 @@ Player.prototype._updateInput = function () {
 	}
 }
 
-Player.prototype._startDragging = function(pointer){
+Player.prototype._startDragging = function (pointer) {
+	if (this.isAbovewater) return;
+
 	this.isDragging = true;
 
 	this.arrow.x = this.arrow.body.x = pointer.worldX;
 	this.arrow.y = this.arrow.body.y = pointer.worldY;
 
 	this.arrow.alpha = 1;
+
+	this.body.angularVelocity = 0;
 
 	// Enable the spring
 	this.spring.stiffness = this.stiffness;
@@ -82,18 +105,30 @@ Player.prototype._stopDragging = function(){
 }
 
 Player.prototype._updateArrow = function () {
-	
+
 	if (this.isDragging) {
 		var p2 = this.arrow;
 		var p1 = this;
 
-		this.arrow.body.rotation = this.body.rotation = this.body.data.rotataion = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+		var rot = Math.atan2(p2.y - p1.y, p2.x - p1.x);
 
-		this.arrow.x = this.arrow.body.x = this.game.input.activePointer.worldX;
-		this.arrow.y = this.arrow.body.y = this.game.input.activePointer.worldY;
+		this.arrow.body.rotation = this.body.rotation = rot;
+
+		this.arrow.body.x = this.game.input.activePointer.worldX;
+		this.arrow.body.y = this.game.input.activePointer.worldY;
 	} else {
-		this.arrow.x = this.arrow.body.x = this.x;
-		this.arrow.y = this.arrow.body.y = this.y;
+		this.arrow.x = this.x;
+		this.arrow.y = this.y;
+
+		if ((Math.abs(this.y - this.game.height) < 2) && this.body.velocity.y < 2) {
+			this.body.rotation = 0;
+			//this.body.setZeroVelocity();
+		} else {
+			this.body.angularVelocity = 0;
+			this.body.rotation = Math.atan2(-this.body.velocity.y, -this.body.velocity.x);
+		}
+
+		
 	}
 }
 
@@ -115,24 +150,24 @@ Player.prototype._updateGravity = function () {
 Player.prototype._fixVelocity = function () {
 	if (this.body.velocity.y > 0) {
 		if (this.body.velocity.y > this.maxVelocity.y) {
-			this.body.velocity.y = this.maxVelocity.y;
+			//this.body.velocity.y = this.maxVelocity.y;
 			//console.log('maxed y');
 		}
 	} else if (this.body.velocity.y < 0) {
 		if (this.body.velocity.y < -this.maxVelocity.y) {
-			this.body.velocity.y = -this.maxVelocity.y;
+			//this.body.velocity.y = -this.maxVelocity.y;
 			//console.log('maxed y');
 		}
 	}
 
 	if (this.body.velocity.x > 0) {
 		if (this.body.velocity.x > this.maxVelocity.x) {
-			this.body.velocity.x = this.maxVelocity.x;
+			//this.body.velocity.x = this.maxVelocity.x;
 			//console.log('maxed x');
 		}
 	} else if (this.body.velocity.x < 0) {
 		if (this.body.velocity.x < -this.maxVelocity.x) {
-			this.body.velocity.x = -this.maxVelocity.x;
+			//this.body.velocity.x = -this.maxVelocity.x;
 			//console.log('maxed x');
 		}
 	}
