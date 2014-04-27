6f646a22fa1b515dabf5ce3e2a3581a11f89d228
jimmyboh <jim@jimmyboh.com>
2014-04-27 17:54:54 -0400

    Removed bad orientation code.

--- game/states/boot.js
+++ game/states/boot.js
@@ -13,33 +13,6 @@ Boot.prototype = {
 	window.Game = this.game;
 	this.game.stage.disableVisibilityChange = true;
 
-	if (this.game.device.desktop)
-        {
-            this.scale.fullscreenScaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.minWidth = 1280;
-            this.scale.minHeight = 720;
-            this.scale.maxWidth = 1920;
-            this.scale.maxHeight = 1080;
-            //this.scale.pageAlignHorizontally = true;
-            //this.scale.pageAlignVertically = true;
-            //this.scale.setScreenSize(true);
-        }
-        else
-        {
-            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.scale.minWidth = 1136;
-            this.scale.minHeight = 640;
-            this.scale.maxWidth = 1920;
-            this.scale.maxHeight = 1080;
-            //this.scale.pageAlignHorizontally = true;
-            //this.scale.pageAlignVertically = true;
-            //this.scale.forceOrientation(true, false);
-            //this.scale.hasResized.add(this.gameResized, this);
-            //this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
-            //this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
-            //this.scale.setScreenSize(true);
-        }
-
     this.game.input.maxPointers = 1;
     this.game.state.start('preload');
   }
