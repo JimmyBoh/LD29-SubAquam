ae0e06d0ce66a3679ae83cf4b119b2aedb5ef798
jimmyboh <jim@jimmyboh.com>
2014-04-27 20:51:52 -0400

    Fixed title screen.

--- dist/js/game.js
+++ dist/js/game.js
@@ -426,10 +426,10 @@ Menu.prototype = {
 
 		var style = { font: '72px Arial Black', fill: '#0ff', align: 'center', stroke:'#000080', strokeThickness: 10 };
 		
-		this.titleText = this.game.add.text(this.game.width/2, 200, 'Aquam Adventure', style);
+		this.titleText = this.game.add.text(this.game.width/2, 200, 'Sub Aquam\nAdventure', style);
 		this.titleText.anchor.setTo(0.5, 0.5);
 
-		this.instructionsText = this.game.add.text(this.game.width/2, 400, 'Click and drag to swim around!\nCollect all the treasures before your air runs out!', { font: '36px Arial', fill: '#ffffff', align: 'center' });
+		this.instructionsText = this.game.add.text(this.game.width/2, 500, 'Click and drag to swim around!\nCollect all the treasures before your air runs out!', { font: '36px Arial', fill: '#ffffff', align: 'center' });
 		this.instructionsText.anchor.setTo(0.5, 0.5);
 	},
 	update: function () {
