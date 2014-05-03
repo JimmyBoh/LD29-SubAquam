4250529a8e58680de8e32ed6ec6dd3d71875afeb
jimmyboh <jim@jimmyboh.com>
2014-05-03 11:19:39 -0400

    Changed name and title screen.

--- dist/js/game.js
+++ dist/js/game.js
@@ -425,8 +425,12 @@ Menu.prototype = {
 
 		var style = { font: '72px Arial Black', fill: '#0ff', align: 'center', stroke:'#000080', strokeThickness: 10 };
 		
-		this.titleText = this.game.add.text(this.game.width/2, 200, 'Sub Aquam\nAdventure', style);
+		this.titleText = this.game.add.text(this.game.width/2, 200, 'Aquam', style);
 		this.titleText.anchor.setTo(0.5, 0.5);
+		
+		style.font = '48px Arial Black'
+		this.subTitleText = this.game.add.text(this.game.width/2, 280, 'Paradise Under the Sea', style);
+		this.subTitleText.anchor.setTo(0.5, 0.5);
 
 		this.instructionsText = this.game.add.text(this.game.width/2, 500, 'Click and drag to swim around!\nCollect all the treasures before your air runs out!', { font: '36px Arial', fill: '#ffffff', align: 'center' });
 		this.instructionsText.anchor.setTo(0.5, 0.5);
